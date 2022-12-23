6d1d4bfd29ac2c05c3127d0c7a867f51b944a746
zurumlab <akanduuj59@yahoo.com>
2022-12-23 11:01:01 +0100

    added stringasarray.py

---
+++ stringasarray.py
@@ -0,0 +1,20 @@
+#This code demonstrate how sting as array of sequence
+
+text = "Python" # creating a string object
+print(len(text)) # checking the size of the string
+print("\n")
+#access each letter in the string using their positions
+print(text[0])
+print(text[1])
+print(text[2])
+print(text[3])
+print(text[4])
+print(text[5])
+
+print("\n")
+
+# just as array can be ittrated, strings can also be ittrated too
+
+for letters in text:
+    print(letters)
+
